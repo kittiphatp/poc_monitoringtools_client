7d6794c79a8b7ffc0bd7dd7e22a64f8dec6a9c59
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2025-05-01 19:05:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ const fetchSend = (url, id) => {
     console.log(`${url}/${id}`, ' :v3')
     fetch(`${url}/${id}`, requestOptions)
       .then((response) => response.json())
-      .then((result) => console.log(JSON.stringify(result)))
+      .then((result) => console.log(result))
       .catch((error) => console.error(error));
 }
 
