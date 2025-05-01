79c7d41bb2e9abce40f019f9e5f07e90ad0565a9
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2025-05-01 17:37:17 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,10 @@ const fetchSend = async (url, id) => {
         const response = await fetch(`${url_fetch}`, {
             method: 'POST',
             headers: {
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                Access-Control-Allow-Headers':    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
             }
         })
         const result = await response.json()
