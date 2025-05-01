48261f513e3d003209b31c992c860bc25be9463e
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2025-05-01 20:26:34 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,8 +69,7 @@ btnAlert.addEventListener('click', () => {
     fetch(`https://poc-monitoringtools-fs-server.vercel.app/api/alerts/${nodeId}`, requestOptions)
       .then(response => {
           if(response.ok){
-              console.log(response)
-              // response.json()
+              return response.json()
           }
       })
       .then(result => JSON.stringify(result, null, 2))
