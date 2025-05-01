50d3df0a0e5e6c4eaacea20230c26a06193c8366
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2025-05-01 18:09:18 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const selectedNode = document.querySelector('#node')
+const btnAlert = document.querySelector('#btnAlert') 
+const btnResolve = document.querySelector('#btnResolve')
+let textareaOutput = document.querySelector('#output')
+
+const fetchSend = async (url, id) => {
+    try{
+        const response = await fetch(`${url}/${id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+            }
+        })
+        const result = await response.json()
+        return result
+    } catch(err){
+        return err
+    }
+}
+
+let nodeId = 0
+
+selectedNode?.addEventListener('change', (e) => {
+    nodeId = Number(e.target.value)
+})
+
+btnAlert?.addEventListener('click', async () => {
+    // const result = await fetchSend('http://localhost:3000/api/alerts', nodeId)
+    const result = await fetchSend('https://poc-monitoringtools-fs-server.vercel.app/api/alerts', nodeId)
+    let resultTxt = JSON.stringify(result, null, 2)
+
+    if (textareaOutput) {
+        textareaOutput.innerHTML = `${resultTxt}`;
+        textareaOutput.classList.remove('textareaResolve')
+        textareaOutput.classList.add('textareaAlert')
+    }
+})
+
+btnResolve?.addEventListener('click', async () => {
+    // const result = await fetchSend('http://localhost:3000/api/resolves', nodeId)
+    const result = await fetchSend('https://poc-monitoringtools-fs-server.vercel.app/api/resolves', nodeId)
+    let resultTxt = JSON.stringify(result, null, 2)
+    
+    if (textareaOutput) {
+        textareaOutput.innerHTML = `${resultTxt}`;
+        textareaOutput.classList.remove('textareaAlert')
+        textareaOutput.classList.add('textareaResolve')
+    }  
+})
