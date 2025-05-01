89936291ca9a663a3ea8444e9fa899f4b02dfd8d
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2025-05-01 20:59:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,8 @@ let textareaOutput = document.querySelector('#output')
 
 const fetchSend = async (url, id) => {
     try{
-        const response = await fetch(`${url}/${id}`, {
-            method: 'POST',
-             mode: 'no-cors'
-        })
+        const response = await fetch(`${url}/${id}`, {method: 'POST', mode: 'no-cors' })
         const result = await response.json()
-        await console.log(result, ' :v5')
         return result
     } catch(err){
         return err
@@ -18,40 +14,15 @@ const fetchSend = async (url, id) => {
 }
 
 let nodeId = 0
+selectedNode.addEventListener('change', (e) => nodeId = Number(e.target.value))
 
-selectedNode?.addEventListener('change', (e) => {
-    nodeId = Number(e.target.value)
-})
+const requestOptions = {method: "POST", mode: 'no-cors'};
 
-// btnAlert?.addEventListener('click', async () => {
-//     const result = await fetchSend('https://poc-monitoringtools-fs-server.vercel.app/api/alerts', nodeId)
-//     let resultTxt = await JSON.stringify(result, null, 2)
-
-//     if (textareaOutput) {
-//         textareaOutput.innerHTML = `${resultTxt}`;
-//         textareaOutput.classList.remove('textareaResolve')
-//         textareaOutput.classList.add('textareaAlert')
-//     }
-// })
-
-const requestOptions = {
-    method: "POST"
-};
 btnAlert.addEventListener('click', () => {
     fetch(`https://poc-monitoringtools-fs-server.vercel.app/api/alerts/${nodeId}`, requestOptions)
-      .then(response => {
-          if(response.ok){
-              return response.json()
-          }
-      })
-      // .then(result => JSON.stringify(result, null, 2))
-      .then(result => {
-          console.log(result)
-          return JSON.stringify(result)
-      })
       .then(txt => {
         if (textareaOutput) {
-            textareaOutput.innerHTML = `${txt}`;
+            textareaOutput.innerHTML = `Sent an alert`;
             textareaOutput.classList.remove('textareaResolve')
             textareaOutput.classList.add('textareaAlert')
         }
@@ -61,10 +32,8 @@ btnAlert.addEventListener('click', () => {
 
 btnResolve.addEventListener('click', async () => {
     const result = await fetchSend('https://poc-monitoringtools-fs-server.vercel.app/api/resolves', nodeId)
-    let resultTxt = JSON.stringify(result, null, 2)
-    
     if (textareaOutput) {
-        textareaOutput.innerHTML = `${resultTxt}`;
+        textareaOutput.innerHTML = `Sent a resolve`;
         textareaOutput.classList.remove('textareaAlert')
         textareaOutput.classList.add('textareaResolve')
     }  
