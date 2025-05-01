7f421cf733220f5b2b4e38597ec2952b3e48f10c
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2025-05-01 17:29:19 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ let textareaOutput = document.querySelector('#output')
 
 const fetchSend = async (url, id) => {
     try{
+        console.log(url)
         const response = await fetch(`${url}/${id}`, {method: 'POST', redirect: 'follow'})
         const result = await response.json()
         return result
